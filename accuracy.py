fd89a1eb478f25d17c29c759be4e7f847311b63e
Damiam Alfaro <149447958+DamiamAlfaro@users.noreply.github.com>
2024-04-15 06:17:16 -0700

    Update accuracy.py

--- accuracy.py
+++ accuracy.py
@@ -2,11 +2,6 @@ from openpyxl import Workbook
 import pandas as pd 
 
 
-
-
-
-
-
 class accuracy():
     def bid_results(self):
         """
@@ -24,6 +19,11 @@ class accuracy():
         return bidders_raw
     
     def stratification(self):
+        """
+        This functions prepares each attribute of the
+        bidder in order to place it within an 
+        excel file for a better visualization.
+        """
         bidders = []
         for bidder_id in self:
             bidder = {}
@@ -42,27 +42,121 @@ class accuracy():
 
 
     def excel_allocation(self,bidders):
+        """
+        This function allocates the "clean" data points
+        from the function above into an excel file."
+        """
         with pd.ExcelWriter(self, engine="openpyxl") as writer:
-            for index, dictionary in enumerate(bidders):
-                df = pd.DataFrame(list(dictionary.items()), columns=["Key","Value"])
-                sheet_name = dictionary.get("Name",f"Sheet_{index+1}")
-                df.to_excel(writer, sheet_name=f"Sheet_{index+1}", index=False)
+            df_all = pd.DataFrame(bidders)
+            df_all.to_excel(writer, index=False, sheet_name="BidResults")
+            for i, bidder in enumerate(bidders,start=1):
+                df_individual = pd.DataFrame([bidder])
+                df_individual.to_excel(writer,index=False,sheet_name=f"Bidder{i}")
 
 
+    def bidder_subcontractors(self):
+        """
+        This function reads and stratifies subcontracor's
+        information from each bidder.
+        """
+        with open(self,"r") as file:
+            read = file.read()
 
+        bidders_subs = read.split("\n\n\n")
+        return bidders_subs
 
+    def subs_allocation(self):
+        """
+        This function allocates the clean subcontractor information
+        into an excel file for visualization.
+        """
+        for i, y in enumerate(self):
+            print(f"Bidder #{i+1}:")
+            print(y)
+            #print("-----------------------------------------")
 
-
-
+    def bidder_line_items(self):
+        """
+        This function cleans and allocates line
+        items data into an excel file.
+        """
+        pass
 
 
 
 
 
 if __name__ == "__main__":
-    bid_results_raw_text = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/OliveGrovePark/OliveGroveBidResults.txt"
-    bid_results_spreadsheet = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/OliveGrovePark/OliveGroveBidResults.xlsx"
+    bid_results_raw_text = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/OliveGrovePark/OliveGroveBidResults.txt" #insert corresponding file
+    bid_results_spreadsheet = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/OliveGrovePark/OliveGroveBidResults.xlsx" #insert corresponding text file
+    bidders_subcontractors = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/OliveGrovePark/OliveGroveBiddersSubs.txt" #insert corresponding text file
+    bidders_line_items = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/OliveGrovePark/OliveGroveBiddersLineItems.txt" #insert corresponding text file
     accuracy.excel_allocation(bid_results_spreadsheet, accuracy.stratification(accuracy.bid_results(bid_results_raw_text)))
+    accuracy.subs_allocation(accuracy.bidder_subcontractors(bidders_subcontractors))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

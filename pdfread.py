3727e3496acafe78923e8ec747811020874e771c
Damiam Alfaro <149447958+DamiamAlfaro@users.noreply.github.com>
2024-04-25 18:31:25 -0700

    Update pdfread.py

--- pdfread.py
+++ pdfread.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import pdfplumber
 from pytesseract import image_to_string
-
+import openpyxl
 
 
 class Specs():
@@ -43,8 +43,40 @@ class Specs():
         asthenosphere = [lava.replace("-","") for lava in mantle]
 
         return asthenosphere
+    
+    # Function 1.3: Renaming the Tabs in the Excel Spreadsheet based on Specs Text
+    def RenameTabs(self,specs):
+        path = self
+        workbook = openpyxl.load_workbook(path)
+        spec_names = specs
+        index_halt = 8
+        if len(workbook.sheetnames) < index_halt + len(spec_names):
+            print("Add more Templates")
+        else:
+            for i, name in enumerate(spec_names):
+                current_tab_index = index_halt + i
+                workbook[workbook.sheetnames[current_tab_index]].title = name
+
+
+            workbook.save(self)
+
+        workbook.close()
 
     
+    # Function 1.4: Allocates list of names to a tab for simplicity 
+    def AllocatingSpecsNames(self,specs):
+        workbook = openpyxl.load_workbook(self)
+        sheet = workbook["BidLayout"]
+
+        for index, value in enumerate(specs,start=1):
+            cell = f"A{index}"
+            sheet[cell] = value
+
+        workbook.save(self)
+
+
+
+
     def SectionsOCR(self):
         with pdfplumber.open(self) as pdf:
             for page in pdf.pages:
@@ -55,15 +87,34 @@ class Specs():
 
 
 if __name__ == "__main__":
-    # Insert Corresponding Specs PDF File
+    # Insert corresponding Specs PDF File
     specs_file = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/GoletaTraintDepot/GoletaSpecSections.pdf"
+    
+    # Insert corresponding Excel Spreadsheet
+    excel_file = "/Users/damiamalfaro/Desktop/Accuracy/Estimate/DataBase/GoletaTraintDepot/GoletaTrainDepot-Spreadsheet.xlsm"
    
     # Function 1: Reads the specs and acquires a list of all spec sections if file is PDF Text-based.
-    Specs.CleaningExtraChar(Specs.SectionsPDFText(specs_file))
+    RawSpecs = Specs.CleaningExtraChar(Specs.SectionsPDFText(specs_file))
+
+    # Function 1.3: Renaming the Tabs
+    #Specs.RenameTabs(excel_file,RawSpecs)
+
+    # Function 1.4: Allocating Specs Names on a Column for VBA
+    Specs.AllocatingSpecsNames(excel_file,RawSpecs)
 
     # Function 2: Reads the specs and acquires a list of all spec sections if file is Image-based.
     #Specs.SectionsOCR(specs_file)
     
+
+
+
+
+
+
+
+
+
+
 
 
 
